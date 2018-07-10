84e394c9be2cb14aefacf91e513e88e6e1b2f2bb
WeaverDyl <dweaver@umd.edu>
2018-07-10 13:35:42 -0400

    actually add 169

--- Majority Element (169)/169.py
+++ Majority Element (169)/169.py
@@ -1,2 +1,18 @@
 # Runtime: 32 ms
 # Beats 99.36% of Python submissions
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        element_multiplicity = {}
+        
+        for num in nums:
+            if num not in element_multiplicity:
+                element_multiplicity[num] = 1
+            else:
+                element_multiplicity[num] += 1
+                
+        return max(element_multiplicity, key=element_multiplicity.get)
