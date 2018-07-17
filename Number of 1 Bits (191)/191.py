7a48d9f4feb7c201172afbb30736603cd1b62aaa
WeaverDyl <dweaver@umd.edu>
2018-07-16 23:52:37 -0400

    add number of 1 bits (191)

---
+++ Number of 1 Bits (191)/191.py
@@ -0,0 +1,17 @@
+# Runtime: 24 ms
+# Beats 97.43% of Python submissions
+
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        
+        while n != 0:
+            if n & 1 == 1:
+                count += 1
+            n = n >> 1
+            
+        return count
